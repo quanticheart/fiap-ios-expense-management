caa9df4494dde02b8f7b9c08c09abb98c7e359af
jonnyz <dev.jonny255d@gmail.com>
2022-06-20 16:17:41 -0300

    update tab bar structure folders

--- expense management/Main/Configurations/ConfigurationViewController.swift
+++ expense management/Main/Configurations/ConfigurationViewController.swift
@@ -1,26 +1,21 @@
 //
-//  MainViewController.swift
+//  ConfigurationViewController.swift
 //  expense management
 //
-//  Created by Jonatas Alves santos on 18/06/22.
+//  Created by Jonatas Alves santos on 19/06/22.
 //
 
 import UIKit
 
-class MainViewController: UITabBarController {
+class ConfigurationViewController: UIViewController {
 
-    @IBOutlet weak var mainTabBar: UITabBar!
-    
-    override func viewDidAppear(_ animated: Bool) {
-        mainTabBar.set(title: "TAB1".localize(), at: 0)
-        mainTabBar.set(title: "TAB2".localize(), at: 1)
-        mainTabBar.set(title: "TAB3".localize(), at: 2)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
+
     /*
     // MARK: - Navigation
 
